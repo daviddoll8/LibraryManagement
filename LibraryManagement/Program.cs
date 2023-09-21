7c9efef0f47c21158ca65f6a6b8ce4983564b0ec
David Doll <doll.david0395@gmail.com>
2023-09-21 17:45:00 -0500

    Added a ServiceExtensions class containing 2 methods, ConfigureCors and ConfigureIISIntegration. ConfigureCors configures the cross origin resource sharing options for now to allow any origin any http method, and any header. ConfigureIISIntegration sets the IIS options to the default options for now. These services were then added to the container in the Program class and the CorsPolicy was added to the HTTP request pipeline in the Program class.

--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,8 +1,12 @@
+using LibraryManagement.ServiceExtensions;
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,12 +26,18 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
