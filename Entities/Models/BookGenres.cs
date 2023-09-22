d4aedbb794c2a7e6795ebb7d30a746ecc5245a44
David Doll <doll.david0395@gmail.com>
2023-09-22 03:31:30 -0500

    Added 2 new model classes to Entities.Models namespace, Genres.cs and BookGenres.cs. Genres for storing the names of genres for books, and the BookGenres model as the intermediate table for the many to many relationship between books and genres.

---
+++ Entities/Models/BookGenres.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class BookGenres
+{
+    [ForeignKey(nameof(Books))] 
+    public Guid BookId;
+
+    [ForeignKey(nameof(Genres))] 
+    public Guid GenreId;
+
+}
---
+++ Entities/Models/Genres.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Genres
+{
+    [Column("GenreId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Genre name is a required field.")]
+    [MaxLength(40, ErrorMessage = "Maximum length for Genre Name is 40 characters.")]
+    public string? Name { get; set; }
+}
